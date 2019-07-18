45dbaf1f6eadc55acf6fdfb810c22ef37f824321
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 13:03:26 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FilteredFruitList = props =>
       <ul className="fruit-list">
-        {this.props.fruits.map((item, index) => <li key={index}>{item.char}</li>)}
+        {props.fruits.map((item, index) => <li key={index}>{item.char}</li>)}
       </ul>;
 
 export default FilteredFruitList;
