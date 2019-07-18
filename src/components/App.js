66b78a82cfc6a6f83093a4c582801303b504cf07
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 12:41:00 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      filters: [],
+      items: [],
+      selectedFilter: null
+    };
+  }
+
+  componentWillMount() {
+    this.fetchFilters();
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  handleFilterChange = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({ selectedFilter: event.target.value });
+  }
+
+  render() {
+    return (
+      <FruitBasket />
+    );
+  }
+}
+
+export default FruitBasket;
+
+
+
+
+// const App = () => <FruitBasket />;
+// 
+// export default App;
