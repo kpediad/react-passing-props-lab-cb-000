1f475bc4447e352f6956fb86009ea468eb0b7c6a
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 12:49:23 +0000

    Automatically backed up by Learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,29 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api/fruit')
-      .then(response => response.json())
-      .then(items => this.setState({ items }));
-  }
-
-  render() {
-    const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
-
-    return (
+const FilteredFruitList = props =>
       <ul className="fruit-list">
-        {list.map((item, index) => <li key={index}>{item.char}</li>)}
-      </ul>
-    );
-  }
-}
+        {this.props.fruits.map((item, index) => <li key={index}>{item.char}</li>)}
+      </ul>;
 
 export default FilteredFruitList;
